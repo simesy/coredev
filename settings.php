45d4119bc611a67a5538d5d1a8bd6476b99b50ae
sime <simon@hobbs.id.au>
2020-05-19 13:10:02 +1000

    Sample settings.php

---
+++ settings.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * A quick start lando settings.php to put in sites/default.
+ */
+
+$databases['default']['default'] = [
+  'database' => 'drupal9',
+  'username' => 'drupal9',
+  'password' => 'drupal9',
+  'prefix' => '',
+  'host' => 'database',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+];
+$settings['install_profile'] = 'minimal';
+
