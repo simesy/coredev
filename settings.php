bd4b3a31732e009b223c0eb5bccbe231f4b3e143
sime <simon@hobbs.id.au>
2020-07-14 15:44:42 +1000

    Add config reference to settings.php

--- settings.php
+++ settings.php
@@ -4,6 +4,8 @@
  * A quick start lando settings.php to put in sites/default.
  */
 
+$settings["config_sync_directory"] = '../config';
+
 $databases['default']['default'] = [
   'database' => 'drupal9',
   'username' => 'drupal9',
